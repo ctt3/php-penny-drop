85e1b6a670eec69d69144867f6778b251beb748c
Charlie <ctt3>
2015-11-14 16:50:48 -0500

    update documentation

--- database.class.php
+++ database.class.php
@@ -3,6 +3,8 @@ class Database {
 	protected static $connection;
 
 	public static function connect(){
+		// Action: Parses config file,
+		//         Instantiates connection to database
 		$config = parse_ini_file('config.ini');
 		self::$connection = new mysqli('127.0.0.1', $config['username'], $config['password'], $config['dbname']);
 		if (self::$connection->connect_errno) {
@@ -12,10 +14,14 @@ class Database {
 	}
 
 	public static function disconnect(){
+		// Action: Closes connection to database
 		self::$connection->close();
 	}
 
 	public static function execute_query($sql){
+		// Input: String sql query
+		// Action: Opens connection, queries db, closes connection
+		// Output: Query result
 		self::connect();
 		$result = self::$connection->query($sql);
 		if ($result === false){echo self::$connection->error;}
@@ -24,6 +30,9 @@ class Database {
 	}
 
 	public static function execute_insert_query($sql){
+		// Input: String sql query
+		// Action: Opens connection, inserts into db, closes connection
+		// Output: Id of inserted record
 		self::connect();
 		$result = self::$connection->query($sql);
 		if ($result === false){
@@ -36,6 +45,9 @@ class Database {
 	}
 
 	public static function insert($table, $columns, $values){
+		// Input: String table-name, String listed columns, String listed values
+		// Action: Builds and executes insert sql, Retrieves newly inserted record
+		// Output: New record
 		$sql = "insert into " . $table . " (" . $columns . ") Values (" . $values . ")";
 		$result = self::execute_insert_query($sql);
 		if($result != false) {
@@ -44,6 +56,9 @@ class Database {
 	}
 
 	public static function update($table, $column_values, $id){
+		// Input: String table-name, String columns = values, String id
+		// Action: Builds and executes update sql, Retrieves newly updated record
+		// Output: Updated record
   	$sql = "update " . $table . " set " . $column_values . " where id = " . $id;
 		$result = self::execute_query($sql);
 		if($result != false) {
@@ -52,12 +67,18 @@ class Database {
 	}
 
 	public static function delete($table, $id){
+		// Input: String table-name, String id
+		// Action: Builds and executes delete sql
+		// Output: Query result
 		$sql = "delete from " . $table . " where id = " . $id;
 		$result = self::execute_query($sql);
 		return $result;
 	}
 
 	public static function select($table, $conditions){
+		// Input: String table-name, String where-conditions
+		// Action: Builds and executes select sql
+		// Output: Array of selected records
 		$sql = "select * from " . $table . " where " . $conditions;
 		$result = self::execute_query($sql);
 		if($result != false) {
@@ -68,6 +89,9 @@ class Database {
 	}
 
 	public static function get_column_names($table){
+		// Input: String table-name
+		// Action: Builds and executes show columns sql
+		// Output: Array of column names
 		$sql = "show columns from " . $table;
 		$result = self::execute_query($sql);
 		if($result != false) {
--- model_record.class.php
+++ model_record.class.php
@@ -3,6 +3,8 @@
 class ModelRecord {
   //**Class methods
   public static function index(){
+    // Action: Retrieve all records in table, wrap records into objects
+    // Output: Array of objects
     $table_name = strtolower( static::class );
     $result = Database::select($table_name, "1");
     $objects = array();
@@ -15,6 +17,9 @@ class ModelRecord {
   }
 
   public static function find($id){
+    // Input: String id
+    // Action: Retrieve record with id, wrap into object
+    // Output: Object
     $table_name = strtolower( static::class );
     $result = Database::select($table_name, "id = " . $id)[0];
     $obj = new static();
@@ -23,6 +28,9 @@ class ModelRecord {
   }
 
   public static function create($attribute_hash){
+    // Input: Associative Array [String column] => String value
+    // Action: Create record, wrap returned record in object
+    // Output: Object
     $table_name = strtolower( static::class );
     $columns = "";
     $values = "";
@@ -45,20 +53,28 @@ class ModelRecord {
   }
 
   public static function delete($id){
+    // Input: String id
+    // Action: Delete record with id
+    // Output: Query result
     $table_name = strtolower( static::class );
     return Database::delete($table_name, $id);
   }
 
   public static function attribute_names(){
+    // Action: Get column names for table
+    // Output: Array of column names
     $table_name = strtolower( static::class );
     return Database::get_column_names($table_name);
   }
 
   public static function assign_instance_variables($obj, $query_result){
+    // Input: Object instance, Query result
+    // Action: assign record values to object instance variables
     foreach($query_result as $attribute => $value) $obj->$attribute = $value;
   }
 
   public static function display_index_table(){
+    // Action: Draw HTML for table of all records in table
     $objects = static::index();
     $attributes = static::attribute_names();
     echo "<table border=1><tr>";
@@ -74,6 +90,8 @@ class ModelRecord {
 
   //**Instance methods
   public function update_attributes($attribute_hash){
+    // Input: Associative Array [String column] => String value
+    // Action: Update record to new values, reassign instance variables to current object
     $table_name = strtolower( static::class );
     $values = "";
     $last_value = end($attribute_hash);
@@ -84,6 +102,13 @@ class ModelRecord {
     }
     $result = Database::update($table_name, $values, $this->id);
     static::assign_instance_variables($this, $result);
+  }
+
+  public function self_destruct(){
+    // Action: Delete record from db
+    // Output: Query result
+    $table_name = strtolower( static::class );
+    return Database::delete($table_name, $this->$id);
   }
 }
 
